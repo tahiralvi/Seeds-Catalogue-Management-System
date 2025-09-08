0aac6b6b37a49dfb076c4030f58e37f40af54f8f
tahiralvi <engtahiralvi@gmail.com>
2025-09-08 03:00:28 -0700

    Add any background picture related to your project.
    Add any background picture related to your project.

--- FinalYearProject/obj/Debug/net8.0/FinalYearProject.AssemblyInfo.cs
+++ FinalYearProject/obj/Debug/net8.0/FinalYearProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalYearProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcfde45298395745b6dce7ee4cd0ae8d16ba3f63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffcada4cc4b447df16a3bdee8dcc4b6997e2ac0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalYearProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalYearProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
