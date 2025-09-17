6321c3add6f2b088e5a668a795e7083a558bf7ac
tahiralvi <engtahiralvi@gmail.com>
2025-09-17 16:57:25 +0500

    Add vertical scrollbar to the main index page to avoid the records get overlapped with fotter
    Add vertical scrollbar to the main index page to avoid the records get overlapped with fotter

--- FinalYearProject/obj/Debug/net8.0/FinalYearProject.AssemblyInfo.cs
+++ FinalYearProject/obj/Debug/net8.0/FinalYearProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalYearProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3334d98b08443a0d978f21df1547dcfd684ab724")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92caf8225c7d315d8b33d0df5853d65a681a3eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalYearProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalYearProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
