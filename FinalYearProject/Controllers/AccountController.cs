27c32fdb5d5240089d687eb01a4e62167abf74a4
tahiralvi <engtahiralvi@gmail.com>
2025-10-17 15:33:24 +0500

    Add Agents, categories and Accounts controller to the project
    Add Agents, categories and Accounts controller to the project

---
+++ FinalYearProject/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalYearProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FinalYearProject/Controllers/AgentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalYearProject.Controllers
+{
+    public class AgentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
