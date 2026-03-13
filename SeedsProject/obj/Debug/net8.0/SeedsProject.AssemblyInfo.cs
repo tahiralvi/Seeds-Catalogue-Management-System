12e35b0c90b2140aa9234cf0843fc1f37015fe34
Tahir Alvi <engtahiralvi@gmail.com>
2026-03-13 21:22:26 +0500

    Format layout CSS and update build metadata
    Reformat Views/Shared/_Layout.cshtml.css (indentation changes only, no behavioral changes). Update generated build artifacts: AssemblyInformationalVersion in SeedsProject.AssemblyInfo.cs, SeedsProject.AssemblyInfoInputs.cache, SeedsProject.sourcelink.json, and rpswa.dswa.cache.json to reflect a new build/commit fingerprint.

--- SeedsProject/obj/Debug/net8.0/SeedsProject.AssemblyInfo.cs
+++ SeedsProject/obj/Debug/net8.0/SeedsProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeedsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ddb21fbbe307068b71a88fd5162ecde6f8da13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dad0a6404fb7ce67b0068ede2b5cc8a394107f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeedsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeedsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
