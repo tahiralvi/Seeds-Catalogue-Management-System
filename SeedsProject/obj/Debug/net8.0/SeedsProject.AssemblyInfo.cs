8332aaa48c645c7ca2f8bf7f32292d014821e52d
Tahir Alvi <engtahiralvi@gmail.com>
2026-03-24 23:06:54 +0500

    Rebuild: update assembly info and artifacts
    Regenerated build outputs and object caches. Updated AssemblyInformationalVersion to 1.0.0+f120a6addf79878a641a4c79b9f581e10875e0ca and refreshed SourceLink entry to the matching commit, causing updated binaries (bin/) and obj/ cache files.

--- SeedsProject/obj/Debug/net8.0/SeedsProject.AssemblyInfo.cs
+++ SeedsProject/obj/Debug/net8.0/SeedsProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeedsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74265bb22d9f1f5c070cae2990929fbaecdbda33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f120a6addf79878a641a4c79b9f581e10875e0ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeedsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeedsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
