97f6076fd71ecc92d753c97738c0c8c1405d814d
Tahir Alvi <engtahiralvi@gmail.com>
2026-04-20 16:20:32 +0500

    update contact email address
    update contact email address

--- SeedsProject/obj/Debug/net8.0/SeedsProject.AssemblyInfo.cs
+++ SeedsProject/obj/Debug/net8.0/SeedsProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeedsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b65a237940d834ef013ceb1a79178566dca0152")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23650a51548d44c86ae3b04e5be31316ccf31cb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeedsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeedsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
