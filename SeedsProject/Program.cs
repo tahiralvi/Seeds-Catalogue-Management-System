b243c9d78c2ee51546743ad6122cec1594100045
Tahir Alvi <engtahiralvi@gmail.com>
2026-04-25 18:29:12 +0500

    fix issue of - InvalidOperationException: The following sections have been defined but have not been rendered by the page at '/Views/Shared/_Layout.cshtml': 'Styles'. To ignore an unrendered section call IgnoreSection("sectionName").
    fix issue of - InvalidOperationException: The following sections have been defined but have not been rendered by the page at '/Views/Shared/_Layout.cshtml': 'Styles'. To ignore an unrendered section call IgnoreSection("sectionName").

--- SeedsProject/Program.cs
+++ SeedsProject/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllersWithViews();
 // Configure DatabaseSettings from appsettings.json
 builder.Services.Configure<DatabaseSettings>(
     builder.Configuration.GetSection("DatabaseSettings"));
-
+builder.Services.AddControllersWithViews();
 // Register the Services
 builder.Services.AddScoped<ISeedService, SeedService>();
 builder.Services.AddScoped<IAgentService, AgentService>();
--- SeedsProject/obj/Debug/net8.0/SeedsProject.AssemblyInfo.cs
+++ SeedsProject/obj/Debug/net8.0/SeedsProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeedsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bbd9556aab4031f7352155618b8c548c682347b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1214ed6db662badc1a5afd4ebdc48284078dc045")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeedsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeedsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
